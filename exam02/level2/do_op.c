f5a533bcc2d468fedf00c732701af446584b0aef
Edtiger1905 <verytiger1905@gmail.com>
2025-12-05 16:57:33 +0100

    pc personale 17:00

--- exam02/level2/do_op.c
+++ exam02/level2/do_op.c
@@ -3,46 +3,46 @@
 #include <stdio.h>
 #include <string.h>
 
-int do_op(int a,char op,int b)
+/* Calcola il risultato dell'operazione richiesta tra due interi, 
+ * gestendo moltiplicazione, divisione, modulo, somma e sottrazione 
+ * tramite il carattere operatore passato come parametro. */
+int do_op(int a,char op,int b) //prende come input due interi e un carattere operatore
 {
-    int result;
+    int result; //variabile per memorizzare il risultato
 
-    result = 0;
-    if(op == '*')
+    result = 0; //inizializza il risultato a 0
+    if(op == '*') //se l'operatore è '*', moltiplica a e b
         result = a * b;
-    else if(op == '/')
+    else if(op == '/') //se l'operatore è '/', dividi a per b
         result = a / b;
-    else if(op == '%')
+    else if(op == '%') //se l'operatore è '%', calcola il modulo di a per b
         result = a % b;
-    else if(op == '+')
+    else if(op == '+') //se l'operatore è '+', somma a e b
         result = a + b;
-    else if(op == '-')
+    else if(op == '-') //se l'operatore è '-', sottrai b da a
         result = a - b;
-    return (result);
+    return (result); //ritorna il risultato dell'operazione
 }
-
 int main(int ac, char **av)
 {
-    int a;
-    int b;
-    char op = 0;
+    int a; //variabile per il primo operando
+    int b; //variabile per il secondo operando
+    char op = 0; //variabile per l'operatore
 
-    if (ac != 4)
+    if (ac != 4) //controlla se il numero di argomenti è corretto, altrimenti stampa una nuova linea e termina
     {
         printf("\n");
         return (0);
     }
-    /* protezione accesso argv */
-    a = atoi(av[1]);
-    b = atoi(av[3]);
+    a = atoi(av[1]); //converte il primo argomento in intero
+    b = atoi(av[3]); //converte il terzo argomento in intero
 
-    /* se av[2] contiene '*' da qualche parte, usalo; altrimenti usa il primo char */
-    if (strchr(av[2], '*'))
+    if (strchr(av[2], '*')) //controlla se l'operatore è '*', altrimenti assegna l'operatore passato come argomento
         op = '*';
     else
-        op = av[2][0];
+        op = av[2][0]; //assegna l'operatore passato come argomento
 
-    if ((op == '/' || op == '%') && b == 0)
+    if ((op == '/' || op == '%') && b == 0) //
     {
         printf("\n");
         return (0);
